26d45584e828c62593508acf049b92faed1520a2
陈博宇 <terry.chen@cloudkeeper.cn>
2019-04-04 14:34:16 +0800

    添加chart

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,34 +1,27 @@
-import Taro, { Component } from '@tarojs/taro'
+import Taro, { Component } from "@tarojs/taro";
+import { View } from "@tarojs/components";
+import "./index.less";
+import AddChart from "../../component/AddChart";
 
-
-
-
-export default class Index extends Component {
+export default class Add extends Component {
   config = {
-    navigationBarTitleText: '首页',
-  
+    navigationBarTitleText: "多图表结合示例"
+  };
+  componentDidMount() {
+    const chartData = {
+      barData: [709,1917,2455,2610,1719,1433,1544,3285,5208,3372,2484,4078],
+      lineData: [1036,3693,2962,3810, 2519,1915,1748, 4675, 6209,4323,2865,4298]
+    };
+    this.addChart.refresh(chartData);
   }
 
-  state = {
-    
-  }
-
- 
-
-  componentWillMount() {
-    wx.setNavigationBarTitle({
-      title: '修改后的导航栏标题',
-      success: function(res) {
-        // success
-      }
-    })
-  }
+  refAddChart = (node) => this.addChart = node
 
   render() {
     return (
-      <View>
-        222
+      <View className="worldCloud-chart">
+        <AddChart ref={this.refAddChart} />
       </View>
-    )
+    );
   }
 }
